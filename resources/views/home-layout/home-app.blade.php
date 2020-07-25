a231d3e5e5a0695e77a4e7968d246125e6ebc4f2
Julio Panjaitan <panjaitanjulio22@gmail.com>
2020-07-25 15:27:16 +0700

    Pertama

---
+++ resources/views/home-layout/home-app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <title>Proyek Invest </title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="{{ asset('template/coming/css/bootstrap.css') }}" rel="stylesheet" />
+    <link href="{{ asset('template/coming/css/coming-sssoon.css') }} " rel="stylesheet" />
+
+
+    <!--     Fonts     -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+@yield('content')
+{{--    <div class="footer">--}}
+{{--        <div class="container">--}}
+{{--            Made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>. Free download <a href="http://www.creative-tim.com/product/coming-sssoon-page">here.</a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+</div>
+</body>
+<script src="{{ asset('template/coming/js/jquery-1.10.2.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template/coming/js/bootstrap.min.js') }}" type="text/javascript"></script>
+
+</html>
---
+++ resources/views/myhome.blade.php
@@ -0,0 +1,56 @@
+@extends('home-layout.home-app')
+@section('content')
+<nav class="navbar2 navbar-transparent navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#">
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                        Tentang Kami
+                    </a>
+                </li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+</nav>
+<div class="main" style="background-image: url('{{ asset('template/coming/images/bg-yerdev.jpg') }}')">
+
+    <!--    Change the image source '/images/default.jpg' with your favourite image.     -->
+
+    <div class="cover green" data-color="black"></div>
+
+    <!--   You can change the black color for the filter with those colors: blue, green, red, orange       -->
+
+    <div class="container">
+        <h1 class="logo cursive">
+            Coming Soon
+        </h1>
+        <!--  H1 can have 2 designs: "logo" and "logo cursive"           -->
+
+        <div class="content">
+            <h4 class="motto">Tertarik lebih lanjut</h4>
+            <div class="row justify-content-lg-center justify-content-md-center justify-content-center form-inline subscribe" style="font-family: 'Roboto Slab', serif;">
+                <div class="col-auto">
+                    <button class="btn btn-success btn-fill btn-lg"style="padding: 10px 20px; opacity: 0.8; font-size: 17px">Take Over / <br> Pindahkan Utang</button>
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-success btn-fill btn-lg" style="padding: 20px 50px; opacity: 0.8">Investasi</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
